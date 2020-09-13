a149c4de02f6ba62b3399b5fb00528744a28a6aa
Andrew Reese <Andrew.Reese@trojans.dsu.edu>
2020-09-12 19:16:11 -0500

    Created the FakeAppUserRepository class that will implement the IAppUser

---
+++ CSC 470 P3 code/CSC 470 P3 code/FakeAppUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC_470_P3_code
+{
+    class FakeAppUserRepository : IAppUser
+    {
+    }
+}
