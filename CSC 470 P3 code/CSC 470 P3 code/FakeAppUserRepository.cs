8692a0feb1715a3f640ff5cf128ec0fd985691c5
Jaron Burtnett <43459765+HylianAdventurer@users.noreply.github.com>
2020-09-13 13:47:02 -0700

    Added and Implemented IAppUser and FakeAppUserRepository

--- CSC 470 P3 code/CSC 470 P3 code/FakeAppUserRepository.cs
+++ CSC 470 P3 code/CSC 470 P3 code/FakeAppUserRepository.cs
@@ -8,5 +8,72 @@ namespace CSC_470_P3_code
 {
     class FakeAppUserRepository : IAppUser
     {
+        private static Dictionary<string, AppUser> Users;
+
+        public FakeAppUserRepository()
+        {
+            if (Users == null)
+            {
+                Users = new Dictionary<string, AppUser>();
+                Users.Add("JaronBurtnett", new AppUser
+                {
+                    UserName = "JaronBurtnett",
+                    Password = "BadPassword1",
+                    FirstName = "Jaron",
+                    LastName = "Burtnett",
+                    EmailAddress = "jaron.burtnett@trojans.dsu.edu",
+                    IsAuthenticated = false
+                });
+                Users.Add("JoeyHopkins", new AppUser
+                {
+                    UserName = "JoeyHopkins",
+                    Password = "BadPassword2",
+                    FirstName = "Joey",
+                    LastName = "Hopkins",
+                    EmailAddress = "joey.hopkins@trojans.dsu.edu",
+                    IsAuthenticated = false
+                });
+                Users.Add("AndrewReese", new AppUser
+                {
+                    UserName = "AndrewReese",
+                    Password = "BadPassword3",
+                    FirstName = "Andrew",
+                    LastName = "Reese",
+                    EmailAddress = "andrew.reese@trojans.dsu.edu",
+                    IsAuthenticated = false
+                });
+            }
+        }
+
+        public List<AppUser> GetAll()
+        {
+            List<AppUser> users = new List<AppUser>();
+            foreach (KeyValuePair<string, AppUser> user in Users) users.Add(user.Value);
+            return users;
+        }
+
+        public AppUser GetByUserName(string UserName)
+        {
+            AppUser user;
+            Users.TryGetValue(UserName, out user);
+            return user;
+        }
+
+        public bool Login(string UserName, string Password)
+        {
+            AppUser user;
+            Users.TryGetValue(UserName, out user);
+            if (user == null) return false;
+            if (user.Password != Password) return false;
+            SetAuthentication(UserName, true);
+            return true;
+        }
+
+        public void SetAuthentication(string UserName, bool IsAuthenticated)
+        {
+            AppUser user;
+            Users.TryGetValue(UserName, out user);
+            user.IsAuthenticated = IsAuthenticated;
+        }
     }
 }
---
+++ CSC 470 P3 code/CSC 470 P3 code/IAppUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC_470_P3_code
+{
+    interface IAppUser
+    {
+        bool Login(string UserName, string Password);
+        List<AppUser> GetAll();
+        void SetAuthentication(string UserName, bool IsAuthenticated);
+        AppUser GetByUserName(string UserName);
+    }
+}
